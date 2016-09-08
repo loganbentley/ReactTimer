3aea643dc2209d8c6e3242ad926ce603fd37b8c5
loganbentley <loganbentley22@gmail.com>
2016-09-07 20:11:36 -0600

    first commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+	render: function() {
+		return (
+			<div className="top-bar">
+				<div className="top-bar-left">
+					<ul className="menu">
+						<li className="menu-text">React Timer App</li>
+						<li>
+							<IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+						</li>
+						<li>
+							<Link to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>CountDown</Link>
+						</li>
+					</ul>
+				</div>
+				<div className="top-bar-right">
+					<ul className="menu">
+						<li className="menu-text">
+							Created by <a href="https://github.com/loganbentley" target="_blank">Logan Bentley</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		);
+	}
+});
+
+
+module.exports = Nav;
